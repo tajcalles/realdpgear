280e69365c5081b4599419a3d8775806c3cd98c3
Taj Calles <tajcalles@gmail.com>
2017-12-02 11:10:51 -0700

    11:10

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root 'accounts#index'
 
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+}
   resources :accounts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
