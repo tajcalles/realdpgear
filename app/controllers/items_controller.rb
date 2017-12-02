759e261437b78b1b25ff5d9e07bd65ce2ea8d148
Dan Actor <danltactor@gmail.com>
2017-12-02 11:17:16 -0700

    update items controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,15 +1,35 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
+    @items = Item.all
   end
 
   def show
   end
 
   def new
+    @item = Item.new
   end
 
   def edit
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to items_index_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to items_index_path
+    else
+      render :edit
+    end
   end
 
   private
@@ -17,5 +37,13 @@ class ItemsController < ApplicationController
   def destroy
     @item.destroy
     redirect_to items_index_path
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def item_params
+    params.require(:item).permit(:name, :price, :description)
   end
 end
