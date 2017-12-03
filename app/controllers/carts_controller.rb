93772f64c6a1c64d4f6943b733323cb21963b500
Dan Actor <danltactor@gmail.com>
2017-12-02 22:51:27 -0700

    updating carts CRUD start to master

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,13 +1,54 @@
 class CartsController < ApplicationController
+  before_action :set_user
+  before_action :set_cart, only: [:show, :edit, :update]
+
   def index
+    @carts = Cart.all
   end
 
   def show
   end
 
   def new
+    @cart = Cart.new
   end
 
   def edit
+  end
+
+  def create
+    @cart = Cart.new(cart_params)
+    if @cart.save
+      redirect_to items_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @cart.update(cart_params)
+      redirect_to items_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    Cart.find(params[:id]).destroy
+    redirect_to carts_path
+  end
+
+  private
+
+  def set_cart
+    @cart = Cart.find(params[:id])
+  end
+
+  def set_user
+    @user = User.find(params[:board_id])
+  end
+
+  def cart_params
+    params.require(:cart).permit(:name, :products, :total)
   end
 end
